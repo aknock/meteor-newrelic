dca68a944d7aca1cb188e24cdb2d831f5c1c8706
Antoine KNOCKAERT <antoine@yper.fr>
2016-08-22 22:11:39 +0200

    Updated plugin on atmosphere

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aknock:newrelic',
-  version: '1.29.0',
+  version: '1.29.1',
   // Brief, one-line summary of the package.
   summary: 'Newrelic package for meteor',
   // URL to the Git repository containing the source code for this package.
